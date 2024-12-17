f2b7dca03fa46dad2f5b610f245bcd169a91d46f
Ivo Dursi <ivo_meggetto@hotmail.com>
2024-12-17 18:50:15 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/seek/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.seek_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:seek_app/app/app.dart';
+import 'package:seek_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
